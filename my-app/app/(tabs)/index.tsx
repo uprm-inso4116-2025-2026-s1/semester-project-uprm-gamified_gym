faa3eee6ac12a94a441a72a7fe6b3e0ebc3a3e10
estefania-2817 <estefania.roca@upr.edu>
2025-10-24 17:18:10 -0400

    completed index page

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, ImageBackground, TouchableOpacity  } from "react-native";
 
 type RootStackParamList = {
   Home: undefined;
@@ -22,55 +22,140 @@ export default function index() {
   const navigation = useNavigation<HomeScreenNavigationProp>();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Index Page</Text>
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate("Login")}
-      />
-      <Button
-        title="Go to SignUp"
-        onPress={() => navigation.navigate("SignUp")}
-      />
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate("Home")}
-      />
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate("Profile")}
-      />    
-      <Button
-        title="Go to Settings"
-        onPress={() => navigation.navigate("Settings")}
-      />
-      <Button
-        title="Go to Password"
-        onPress={() => navigation.navigate("Password")}
-      />
-      <Button
-        title = "Go to Exercises"
-        onPress={() => navigation.navigate("ExerciseLog")}
-        />
-
-    </View>
+      <View style={styles.border}>
+        <ImageBackground 
+          source={require('../../assets/images/indexbackground.jpg')} 
+          style={styles.background}
+          resizeMode="cover">
+          <View style={styles.container}>        
+            <Text style={styles.title}>Welcome to our</Text>
+            <Text style={styles.title}>mobile Gamified app</Text>
+            <Text style={styles.subtitle1}>If you do not have an account with us, please register here</Text>
+            <TouchableOpacity 
+              style={styles.signupbutton}
+              onPress={() => navigation.navigate("SignUp")}
+            >
+            <Text style={styles.buttonText}>Sign Up</Text>
+            </TouchableOpacity>
+            {/* <Button style={styles.signupbutton}
+              title="Go to SignUp"
+              onPress={() => navigation.navigate("SignUp")}
+            /> */}
+            <Text style={styles.subtitle2}>If you have an account with us, please login here</Text>
+            {/* <Button style={styles.loginbutton}
+              title="Go to Login"
+              onPress={() => navigation.navigate("Login")}
+            /> */}
+            <TouchableOpacity 
+              style={styles.loginbutton}
+              onPress={() => navigation.navigate("Login")}
+            >
+            <Text style={styles.buttonText}>Log In</Text>
+            </TouchableOpacity>
+            {/* <Button
+              title="Go to Home"
+              onPress={() => navigation.navigate("Home")}
+            />
+            <Button
+              title="Go to Profile"
+              onPress={() => navigation.navigate("Profile")}
+            />    
+            <Button
+              title="Go to Settings"
+              onPress={() => navigation.navigate("Settings")}
+            />
+            <Button
+              title="Go to Password"
+              onPress={() => navigation.navigate("Password")}
+            />
+            <Button
+              title = "Go to Exercises"
+              onPress={() => navigation.navigate("ExerciseLog")}
+              /> */}
+          </View>
+        </ImageBackground>
+      </View>
   );
 } 
 
+const BLUE = "#2F80FF";
+
 const styles = StyleSheet.create({
+  background: {
+    top: 10,
+    position: 'absolute',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
+    borderColor: BLUE,
+    borderWidth: 5,
+    borderRadius: 20,
+  },
+  border: {
+    position: 'absolute',
+    height: '98%',
+    width: '100%',
+    borderWidth: 25,
+    borderColor: BLUE,
+    backgroundColor: BLUE,
+    padding: 10,
+  },
   container: {
     flex: 1,
+    width: '100%',
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff", // make sure background is visible
+    borderWidth: 5,
+    borderRadius: 20,
+    padding: 20,
   },
   title: {
-    fontSize: 24,
-    color: "teal",
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "white",
     marginBottom: 10,
+    top: -80,
+    width: '100%',
+    textAlign: 'center',
   },
-  subtitle: {
-    fontSize: 18,
-    color: "black",
+  subtitle1: {
+    width: '80%',
+    textAlign: 'center',
+    top: -60,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
   },
+  subtitle2: {
+    width: '80%',
+    textAlign: 'center',
+    top: 20,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+  signupbutton: {
+    top: -50,
+    width: 140,
+    height: 40,
+  },
+  loginbutton: {
+    top: 30,
+    width: 140,
+    height: 40,
+  },
+  buttonText: {
+    textAlign: 'center',
+    width: '100%',
+    padding: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    backgroundColor: BLUE,
+    borderRadius: 10,
+  }
 });
